c7c7ebe8cbc26ea6e1f15c0ad4b1c6d282cc869a
ravibabuvadde <79750913+ravibabuvadde@users.noreply.github.com>
2022-08-17 17:45:59 +0530

    Add THANOS SNAP to terminate the program

--- main.py
+++ main.py
@@ -29,23 +29,32 @@ def main():
         fps = int(round(1 / (ct - pt)))
         pt = ct
         cv2.putText(img, "FPS : {}".format(fps), (0, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
-
+        break_out = False
         if data.multi_hand_landmarks:
             for x in data.multi_hand_landmarks:
                 pointx = x.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x
                 pointy = x.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y
-                wristx = x.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].x
-                wristy = x.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].y
+                middlex = x.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].x
+                middley = x.landmark[mpHands.HandLandmark.MIDDLE_FINGER_TIP].y
                 thumbx = x.landmark[mpHands.HandLandmark.THUMB_TIP].x
                 thumby = x.landmark[mpHands.HandLandmark.THUMB_TIP].y
                 cv2.circle(img, (int(pointx * vidw), int(pointy * vidh)), 10, (255, 255, 0), -1)
                 cv2.circle(img, (int(thumbx * vidw), int(thumby * vidh)), 10, (255, 0, 255), -1)
-                pag.moveTo(wristx * screen_width * 1.3, wristy * screen_height * 1.3)
+                pag.moveTo(middlex * screen_width * 1.3, middley * screen_height * 1.3)
 
+                # THANOS SNAP break if thumb and middle finger are close
+                if abs(math.sqrt((middlex * screen_width - thumbx * screen_width) ** 2 + (
+                        middley * screen_height - thumby * screen_height) ** 2)) < 50:
+                    break_out = True
+                    break
+
+                # click if thumb finger is close to index finger
                 if abs(math.sqrt((pointx * screen_width - thumbx * screen_width) ** 2 + (
                         pointy * screen_height - thumby * screen_height) ** 2)) < 50:
                     pag.click()
-                    pag.sleep(1)
+                    break
+            if break_out:
+                break
 
         cv2.imshow('Gesture Controlled Mouse', img)
         cv2.waitKey(1)
